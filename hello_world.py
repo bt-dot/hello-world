37130dc8dc2b7074b16235926f5dff53f3c3cf45
Bruce Tang <55170799+bt-dot@users.noreply.github.com>
2021-02-17 13:54:26 -0500

    Create hello_world.py
    Sample code PQ0

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+"""
+Our first Python test program.
+Team members: Bruce Tang, Danny Little, August Wadlington
+"""
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
