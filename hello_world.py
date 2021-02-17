aaafac256ab9cc21467f704ff3426d0572855c51
Danny Little <dannyl9979@gmail.com>
2021-02-17 16:24:39 -0500

    Update hello_world.py
    Made code unnecessarily complicated and terse.

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,13 @@ Our first Python test program.
 Team members: August Wadlington, Bruce Tang, Danny Little
 """
 
+
 def main():
-    print("Hello, world!")
+    my_cool_variable = 'Hello world!'[::-1]
+    my_cool_variable_to_list = list(my_cool_variable)
+    my_cool_variable_to_list.reverse()
+    print(''.join(my_cool_variable_to_list))
+
 
 if __name__ == "__main__":
     main()
