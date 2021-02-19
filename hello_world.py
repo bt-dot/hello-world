3bd89853481eff870829089f513cde0991a20d94
August <awadling@bowdoin.edu>
2021-02-18 21:43:49 -0500

    counting feature
    Added in a function to count how many attempts it takes to find the right letters, along with a bonus functionality for the .09% chance of getting a perfect "Hello World!" string with no incorrect letters ever appearing

--- hello_world.py
+++ hello_world.py
@@ -9,14 +9,19 @@ def main():
     my_cool_variable = ''
     target = 'Hello world!'
     i = 0
+    misfires = 0
     while my_cool_variable != 'Hello world!':
         r = randint(32, 128)
         to_add = chr(r)
         if to_add == target[i]:
             my_cool_variable += to_add
             i += 1
+        elif to_add != target[i]:
+            misfires += 1
     print(my_cool_variable)
-
+    print("Misses: ", misfires)
+    if misfires == 12:
+        print("Are you cheating?")
 
 if __name__ == "__main__":
     main()
